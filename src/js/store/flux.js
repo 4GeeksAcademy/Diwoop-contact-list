1e7c664f668e4ad2b71d29b508521cf46c90526b
Diana <diana.ep.dev@gmail.com>
2024-09-16 23:32:44 +0200

    Update.js

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -6,19 +6,19 @@ const getState = ({ getStore, getActions, setStore }) => {
 				"phone": "",
 				"email": "",
 				"address": ""
-			}],//Aqui guardo los datos del GET
+			}],
 			contact: {},
-			slug: "AJPadillo"
+			slug: "DianaEstallo"
 		},
 		actions: {
-			loadSomeData: async () => {//ACTUALIZA LOS DATOS DE LA API (NOS TRAE LOS DATOS)
-				const response = await fetch("https://playground.4geeks.com/contact/agendas/AJPadillo/contacts", { method: "GET" });
+			loadSomeData: async () => {
+				const response = await fetch("https://playground.4geeks.com/contact/agendas/DianaEstallo/contacts", { method: "GET" });
 				const data = await response.json();
 				setStore({ agenda: data.contacts })
 			},
 			crearAgenda: async () => {
 				try {
-					const response = await fetch("https://playground.4geeks.com/contact/agendas/AJPadillo", {
+					const response = await fetch("https://playground.4geeks.com/contact/agendas/DianaEstallo", {
 						method: "POST",
 						headers: {
 							"Content-Type": "application/json",
@@ -33,7 +33,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			getAgenda: async () => {
 				try {
-					const response = await fetch("https://playground.4geeks.com/contact/agendas/AJPadillo")
+					const response = await fetch("https://playground.4geeks.com/contact/agendas/DianaEstallo")
 					if (response.status != 200) {
 						console.log(response.status)
 					}
@@ -44,32 +44,35 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log(error);
 				}
 			},
-			nuevoContacto: async (name, phone, address, email, navigate) => {
-				console.log(name, phone, address, email);
+			nuevoContacto: async (name, phone, email, address, navigate) => {
+				const capitalizedName = name.replace(/\b\w/g, (char) => char.toUpperCase());
+				const capitalizedAddress = address.replace(/\b\w/g, (char) => char.toUpperCase());
+			
 				try {
 					const cuerpo = JSON.stringify({
-						"name": name,
+						"name": capitalizedName,
 						"phone": phone,
 						"email": email,
-						"address": address
+						"address": capitalizedAddress
 					});
-					const uri = "https://playground.4geeks.com/contact/agendas/AJPadillo/contacts";
+			
+					const uri = "https://playground.4geeks.com/contact/agendas/DianaEstallo/contacts";
 					const requestsOptions = {
 						method: "POST",
 						headers: { "Content-Type": "application/json" },
 						body: cuerpo,
-					}
+					};
 					const response = await fetch(uri, requestsOptions);
 					const data = await response.json();
 					getActions().loadSomeData();
 					navigate("/");
 				} catch (error) {
-					console.log(error)
+					console.log(error);
 				}
 			},
 			borrarContacto: async (idContacto) => {
 				const requestsOptions = { method: "DELETE" };
-				const response = await fetch(`https://playground.4geeks.com/contact/agendas/AJPadillo/contacts/${idContacto}`, requestsOptions);
+				const response = await fetch(`https://playground.4geeks.com/contact/agendas/DianaEstallo/contacts/${idContacto}`, requestsOptions);
 				if (response.ok) {
 					const store = getStore();
 					const updateContacts = store.agenda.filter(contact => contact.id != idContacto);
@@ -79,7 +82,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 			actualizarContacto: async (contact, navigate) => {
 				try {
-					const uri = `https://playground.4geeks.com/contact/agendas/AJPadillo/contacts/${contact.id}`;
+					const uri = `https://playground.4geeks.com/contact/agendas/DianaEstallo/contacts/${contact.id}`;
 					const options = {
 						method: "PUT",
 						headers: {
@@ -101,6 +104,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log(error);
 					throw error;
 				}
+			},
+			saveContact: (contact) => {setStore({contact: contact})}
+		}
+	};
+};
+
+export default getState;
 			},
 			saveContact: (contact) => {setStore({contact: contact})}
 		}
